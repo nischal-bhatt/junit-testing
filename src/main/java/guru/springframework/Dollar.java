a3cbcad655709d19da45841f2945ebee73f98dac
nischal <nischal.bhatt.87@gmail.com>
2022-04-10 16:12:59 +0800

    commiting some changes

---
+++ src/main/java/guru/springframework/Dollar.java
@@ -0,0 +1,29 @@
+package guru.springframework;
+
+public class Dollar {
+
+	private int amount;
+	
+	public Dollar(int amount)
+	{
+		this.amount = amount;
+	}
+	
+	Dollar times(int multiplier)
+	{
+	    
+	    return new Dollar(amount * multiplier);
+	}
+	
+	public boolean equals(Object object)
+	{
+		Dollar dollar = (Dollar)object;
+		
+		
+		return amount == dollar.amount;
+	}
+
+	
+	
+	
+}
